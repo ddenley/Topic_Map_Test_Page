b4cfdddce192feaf8249cfead2b532ff05b3ca67
Daniel <daniel-denley@live.co.uk>
2023-11-08 21:29:51 +0000

    New Topic Map

---
+++ main.js
@@ -0,0 +1,214 @@
+
+const dataDirectory = './data/test/';
+const urls = {
+    mainMap : dataDirectory + 'mainMap.json',
+    subMaps : dataDirectory + 'subMaps.json',
+    mainModel : dataDirectory + 'mainModel.json',
+    subModel : dataDirectory + 'subModel.json',
+    labelsIndex : dataDirectory + 'labelIndex.json',
+    documents : dataDirectory + 'model.csv',
+    distributionCoordinatorCountry : dataDirectory + 'distributionCoordinatorCountry.json',
+    distributionCoordinatorName: dataDirectory + 'distributionCoordinatorName.json',
+    distributionProjectStatus: dataDirectory + 'distributionProjectStatus.json',
+    distributionEndDate: dataDirectory + 'distributionEndDate.json',
+    trend: dataDirectory + 'distributionStartDate.json',
+}
+
+//Bar values x, y, left-margin
+//distMax - max labels taken from distribution
+const distribution_names = [
+    {distName: 'Coordinator Country', distURL: 'distributionCoordinatorCountry', distMax: 10, barValues: [400, 150, 100]},
+    {distName: 'Coordinator Name', distURL: 'distributionCoordinatorName', distMax: 10, barValues: [700, 150, 450]},
+    {distName: 'Project Status', distURL: 'distributionProjectStatus', distMax: 2, barValues: [400, 150, 100]}
+]
+
+//****Managers****
+
+let PM = tMap.PageManager('div#app', 'E', '2-4-2-l', 'div#header', 'div#footer', 700, 700);
+let DM = tMap.DataManager();
+let SM = tMap.StateManager();
+
+//**** Main Visualizations****
+
+//Main map
+//TODO: Would like to show the normalized distribution as this is more informative
+let mainMap = tMap.BubbleMap(PM.panel1.c, PM.panel1.w, PM.panel1.h)
+    .setBubbleClick(selectMainTopic)
+    .addDefaultText('Loading...', 2, true)
+    .setTooltipChart((t,d)=>{
+        let distNameEntries = getDistNameEntries(SM.state('distrib'));
+        console.log(distNameEntries);
+        let distURL = distNameEntries.distURL;
+        let distMax = distNameEntries.distMax;
+        let barValues_x = distNameEntries.barValues[0];
+        let barValues_y = distNameEntries.barValues[1];
+        let barValues_leftMargin = distNameEntries.barValues[2];
+        let topEntries = topEntriesOnly(DM.getMainTopicDistribEntry(d.topicId, distURL), distMax);
+        tMap.HorizontalBarChart(t, barValues_x, barValues_y)
+            .setMarginLeft(barValues_leftMargin)
+            .render(topEntries);
+})
+
+
+//Sub map
+let subMap = tMap.BubbleMap(PM.panel2.c, PM.panel2.w, PM.panel2.h)
+    .setBubbleClick(selectSubTopic)
+    .addDefaultText('Click on a bubble to see more topics.', 1, true)
+    .setTooltipChart((t,d)=>{
+        let distNameEntries = getDistNameEntries(SM.state('distrib'));
+        console.log(distNameEntries);
+        let distURL = distNameEntries.distURL;
+        let distMax = distNameEntries.distMax;
+        let barValues_x = distNameEntries.barValues[0];
+        let barValues_y = distNameEntries.barValues[1];
+        let barValues_leftMargin = distNameEntries.barValues[2];
+        let topEntries = topEntriesOnly(DM.getSubTopicDistribEntry(d.topicId, distURL), distMax);
+        tMap.HorizontalBarChart(t, barValues_x, barValues_y)
+            .setMarginLeft(barValues_leftMargin)
+            .render(topEntries);
+    })
+
+//Word cloud
+let wordcloud = tMap.WordCloud(PM.panel3.c, PM.panel3.w, PM.panel3.h)
+    .addDefaultText('Click on a bubble to see more labels.', 1, true)
+
+//----Table tooltip----
+let tableTooltip = d => {
+    let fields = [];
+    fields.push(`Relevance: ${Math.floor(d.weight*100)}%`);
+    return fields.join(' - ');
+}
+
+//Document view
+let table = tMap.DocTable(PM.panel4.c, PM.panel4.w, PM.panel4.h)
+    .addDefaultText('Click on a bubble to see the topic top documents.',1,true)
+    .toggleTitle('Top Documents')
+    .setColumnsInfo([
+        {title: 'Acronym', accessor: d=>d.docData.project_acronym, tooltip:tableTooltip},
+        {title:'Title',accessor:d=>d.docData.title, tooltip:tableTooltip},
+        {title:'Objective',accessor:d=>shortenText(d.docData.objective, 200), tooltip:tableTooltip}
+    ])
+    .rowsFilter([0,20,60,90],(e,d)=>{table.render(DM.getTableRows(50, d2=>Math.floor(d2.weight*100)>=d),d)},'Min Relevance')
+
+function shortenText(text, length){
+    return text.length > length ? text.slice(0,length-3) + '...' : text;
+}
+
+//Trend view
+let trend = tMap.TrendChart(PM.panel5.c, PM.panel5.w, PM.panel5.h)
+    .addDefaultText('Click on a bubble to see the topic trend.',1,true)
+
+
+
+//**** Controls****
+
+//Labels Search
+let search = tMap.Search(PM.control1.c, PM.control1.w, PM.control1.h)
+    .setSearchCB(searchLabels)
+
+//Distribution dropdown
+let dropdown = tMap.Dropdown(PM.control2.c, PM.control2.w, PM.control2.h)
+    .setSelectCB(selectDistribution);
+
+
+//****Functions****
+
+//Main topic click function
+function selectMainTopic(e,d){
+    console.log("Selected main topic: ", d);
+    //Update state
+    SM.state('mainTopic', d.topicId);
+    //Render new sub map
+    renderSubMap();
+    //Render wordcloud
+    wordcloud.render(d.labels)
+    //Show table documents
+    DM.setTableRowsMainTopic(SM.state('mainTopic'));
+    table.render(DM.getTableRows(50));
+
+    //Rehighlight from search
+    highlightFromLabelSearch();
+
+    //Render trend
+    trend.render([DM.getMainTopicTrend(d.topicId, sumByYear, trendRange)]);
+}
+
+//Sub topic click function
+function selectSubTopic(e,d){
+    console.log("Selected sub topic: ", d);
+    //Show wordcloud
+    SM.state('subTopic', d.topicId);
+    wordcloud.render(d.labels);
+    //Show table documents
+    DM.setTableRowsSubTopic(SM.state('subTopic'));
+    table.render(DM.getTableRows(50));
+
+    //Rehighlight from search
+    highlightFromLabelSearch();
+}
+
+//Function to render new sub map
+function renderSubMap(){
+    //Set state of subtopic back to none
+    SM.state('subTopic', null);
+    subMap.render(DM.getSubMap(SM.state('mainTopic')));
+}
+
+//Search function
+function searchLabels(query){
+    SM.state('search', query.length > 0 ? query : null);
+    DM.setSearchTerm(SM.state('search'));
+    highlightFromLabelSearch()
+}
+
+//Distribution dropdown function
+function selectDistribution(){
+    console.log("Selected distribution: ", dropdown.getSelected().value);
+    SM.state('distrib', dropdown.getSelected().value);
+}
+
+//Function to highlight from search
+function highlightFromLabelSearch(){
+    let {mainTopicIds, subTopicIds} = DM.getTopicIdsFromSearch();
+    mainMap.highlightBubbles(mainTopicIds);
+    subMap.highlightBubbles(subTopicIds);
+    let labels = DM.getLabelsFromSearch();
+    wordcloud.highlightTexts(labels);
+    let docIds = DM.getDocIdsFromSearch();
+    table.highlightDocs(docIds);
+}
+
+//Helper functions
+function getDistNameEntries(distName){
+    //Get distribution values from distribution_names
+    let dist = distribution_names.find(d=>d.distName==distName);
+    return dist;
+}
+
+function topEntriesOnly(entries, n=10){
+    entries.sort((a,b)=>b.value-a.value);
+    return entries.slice(0,n);
+}
+
+let sumByYear = DM.timeFormatConverter('%Y-%m-%d', '%Y'); // Convert to year only format
+let trendRange = ['%Y-%m-%d', '2011-01-01', '2024-01-01']; // Set the correct range
+
+//****Load and Process Data****
+
+DM.loadAndProcessDataFromUrls(urls).then(()=>{
+    //console.log(DM.data);
+    mainMap.render(DM.data.mainMap);
+
+    // Load distribution into dropdown control
+    dropdown.setOptions(distribution_names.map(d=>{return {value:d.distName, text:d.distName}}));
+
+    //Set default distribution
+    SM.state('distrib', distribution_names[0].distName);
+
+    console.log(DM.data.trend);
+    console.log(DM.getMainTopicTrend('2'));
+    console.log(DM.getMainTopicTrend('5', sumByYear, trendRange));
+
+    // trend.render([DM.getMainTopicTrend('2', sumByYear, trendRange)]);
+
+})
---
+++ main_old.js
@@ -0,0 +1,182 @@
+const dataDirectory = './data/test/';
+const urls = {
+    mainMap : dataDirectory + 'mainMap.json',
+    subMaps : dataDirectory + 'subMaps.json',
+    mainModel : dataDirectory + 'mainModel.json',
+    subModel : dataDirectory + 'subModel.json',
+    labelsIndex : dataDirectory + 'labelIndex.json',
+}
+
+let PM = tMap.PageManager('div#app', 'E', '4-5-1-5-1-b', 'div#header', 'div#footer', 700, 700);
+
+let DM = tMap.DataManager();
+
+let SM = tMap.StateManager();
+
+let mainMap = tMap.BubbleMap(PM.panel1.c, PM.panel1.w, PM.panel1.h)
+    .setBubbleClick(selectMainTopic)
+    .addDefaultText('Loading...', 2, true)
+    .setMargin([40,10,10,10])
+    .toggleButton('TR', 'Test', ()=>{console.log('button test')})
+    .toggleTitle('Topic Map').setMinimumTextSize(2);
+let wordcloud = tMap.WordCloud(PM.panel3.c, PM.panel3.w, PM.panel3.h)
+    .addDefaultText('Click on a bubble to see more labels.', 1, true)
+    .setMargin([40,10,10,10])
+    .toggleTitle('Topic Labels')
+    .setWordClick((e,d)=>{
+        let s = DM.processWordInSearch(d.t, SM.state('search'));
+        search.setValue(s);
+        searchLabels(s.toLowerCase());
+    });
+let subMap = tMap.BubbleMap(PM.panel2.c, PM.panel2.w, PM.panel2.h)
+    .setBubbleClick(selectSubTopic)
+    .addDefaultText('Click on a bubble to see more topics.')
+    .setMargin([40,10,10,10])
+    .toggleButton('TR', 'Test', ()=>{console.log('button test')})
+    .toggleTitle('Topic Map');
+
+let trend = tMap.TrendChart(PM.panel4.c, PM.panel4.w, PM.panel4.h)
+
+
+let tableTooltip = d => {
+    let fields = [];
+    fields.push(`Word Count: ${d.docData.wordCount}`);
+    fields.push(`Relevance: ${Math.floor(d.weight*100)}%`);
+    return fields.join(' - ');
+}
+let tableTooltipChart = (t,d)=>{
+    let v = parseFloat(d.docData.money)
+    tMap.VerticalBarChart(t, 200, 100)
+        .setTicks(3, '.0%')
+        .render([{key:'A',value:v}])
+}
+let table = tMap.DocTable(PM.panel5.c, PM.panel5.w, PM.panel5.h)
+    .addDefaultText('Click on a bubble to see the topic top documents.',1,true)
+    .toggleTitle('Top Documents')
+    .setColumnsInfo([
+        {title:'Title',accessor:d=>d.docData.title,tooltip:tableTooltip,click:selectDoc},
+        {title:'Authors',accessor:d=>d.docData.abstract,tooltip:tableTooltip,click:selectDoc},
+    ])
+    .rowsFilter([0,20,60,90],(e,d)=>{table.render(DM.getTableRows(50, d2=>Math.floor(d2.weight*100)>=d),d)},'Min Relevance')
+
+
+function selectMainTopic(e,d){
+    SM.state('mainTopic', d.topicId);
+    mainMap.selectBubble(SM.state('mainTopic'));
+    wordcloud.render(d.labels);
+    newSubMap();
+    trend.render([DM.getMainTopicTrend(SM.state('mainTopic'), sumByYear, trendsRange)]);
+    DM.setTableRowsMainTopic(SM.state('mainTopic'));
+    table.render(DM.getTableRows(50));
+
+    highlightFromLabelSearch();
+}
+
+function newSubMap(){
+    SM.state('subTopic', null);
+    subMap.render(DM.getSubMap(SM.state('mainTopic')));
+}
+
+function selectSubTopic(e,d){
+    SM.state('subTopic', d.topicId);
+    subMap.selectBubble(SM.state('subTopic'));
+    wordcloud.render(d.labels);
+    trend.render([DM.getMainTopicTrend(SM.state('mainTopic'), sumByYear, trendsRange),
+        DM.getSubTopicTrend(SM.state('subTopic'), sumByYear, trendsRange)]);
+    DM.setTableRowsSubTopic(SM.state('subTopic'));
+    table.render(DM.getTableRows(50));
+
+    highlightFromLabelSearch();
+}
+
+function selectDoc(e,d){
+    // docView.render(d.docData);
+}
+
+let dropdown = tMap.Dropdown(PM.control3.c,PM.control3.w,PM.control3.h)
+    .setSelectCB(selectDistribution);
+
+function selectDistribution(value){
+    SM.state('distrib', value);
+    if(SM.state('distrib') == 'All'){
+        mainMap.setBubblesOpacity([], true);
+        subMap.setBubblesOpacity([], true);
+    } else {
+        mainMap.setBubblesOpacity(DM.getMainTopicsDistribNormPerTopic(SM.state('distrib')));
+        subMap.setBubblesOpacity(DM.getSubTopicsDistribNormPerTopic(SM.state('distrib')));
+    }
+}
+
+let search = tMap.Search(PM.control1.c,PM.control1.w,PM.control1.h)
+    .setSearchCB(searchLabels);
+
+function searchLabels(query){
+    SM.state('search', query.length > 0 ? query : null);
+    DM.setSearchTerm(SM.state('search'));
+    highlightFromLabelSearch();
+}
+
+function highlightFromLabelSearch(){
+    let {mainTopicIds, subTopicIds} = DM.getTopicIdsFromSearch();
+    mainMap.highlightBubbles(mainTopicIds);
+    subMap.highlightBubbles(subTopicIds);
+    let labels = DM.getLabelsFromSearch();
+    wordcloud.highlightTexts(labels);
+    let docIds = DM.getDocIdsFromSearch();
+    table.highlightDocs(docIds);
+}
+
+let menu = tMap.Menu(PM.control4.c,PM.control4.h)
+    .addShare(()=>{
+        return SM.buildURL();
+    })
+    .addScreenshot('#app')
+    .addDownload([{name:'Model data',url:dataDirectory+'model.csv'}])
+    .addModal('About', tMap.aboutTopicMapping())
+
+PM.watch({
+    panel1: mainMap,
+    panel2: subMap,
+    panel3: wordcloud,
+    panel4: trend,
+    panel5: table,
+    control1: search,
+    control3: dropdown,
+    control4: menu
+})
+
+function parseURL(){
+    SM.parseURL();
+    // ...
+}
+
+DM.loadAndProcessDataFromUrls(urls).then(()=>{
+    console.log(DM.data);
+    mainMap.render(DM.data.mainMap);
+
+    let distribValues = DM.getDistributionLabels();
+    distribValues.unshift({value:'All',text:'All'});
+    dropdown.setOptions(distribValues);
+
+    menu.addModelInfo(DM.getMainModelMetadata(), DM.getSubModelMetadata())
+        .addCharts('Models Log-Likelihood', [
+            (m,w,h)=>{
+                tMap.LineChart(m,w,h)
+                    .toggleTitle('Main Model')
+                    .toggleBorder(false)
+                    .setMargin([20,20,60,20])
+                    .setAxesNames('Iterations', 'Log-Likelihood')
+                    .render(DM.getMainLLData().map(d=>{return {x:d.iter,y:d.LL}}))
+            },
+            (m,w,h)=>{
+                tMap.LineChart(m,w,h)
+                    .toggleTitle('Sub Model')
+                    .toggleBorder(false)
+                    .setMargin([20,20,60,20])
+                    .setAxesNames('Iterations', 'Log-Likelihood')
+                    .render(DM.getSubLLData().map(d=>{return {x:d.iter,y:d.LL}}))
+            }
+        ])
+
+    parseURL();
+})
